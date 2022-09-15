a66678bff4ae602f846a8bc232c237986dfa1737
c0nd3v <c0nd3v12@gmail.com>
2022-09-16 00:31:22 +0700

    Initial commit

---
+++ Serilog.Sinks.Channels/ChannelSink.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Serilog.Events;
+using Serilog.Core;
+
+namespace Serilog.Sinks.Channels
+{
+	internal sealed class ChannelSink : ILogEventSink, IDisposable
+	{
+		private ILogEventSink _wrappedSink;
+		private Channel<LogEvent> _logChannel;
+		private Task _workerTask;
+
+		public ChannelSink(ILogEventSink wrappedSink)
+		{
+			_wrappedSink = wrappedSink ?? throw new ArgumentNullException(nameof(wrappedSink));
+			_logChannel = Channel.CreateUnbounded<LogEvent>();
+			_workerTask = PumpAsync();
+		}
+
+		public void Emit(LogEvent logEvent)
+		{
+			_logChannel.Writer.TryWrite(logEvent);
+		}
+
+		public void Dispose()
+		{
+			// Prevent any more events from being added
+			_logChannel.Writer.TryComplete();
+
+			// Allow queued events to be flushed
+			_workerTask.Wait();
+
+			(_wrappedSink as IDisposable)?.Dispose();
+		}
+
+		private async Task PumpAsync()
+		{
+			while (await _logChannel.Reader.WaitToReadAsync())
+			{
+				await foreach (var logEvent in _logChannel.Reader.ReadAllAsync())
+					_wrappedSink.Emit(logEvent);
+			}
+		}
+	}
+}
---
+++ Serilog.Sinks.Channels/LoggerConfigurationChannelExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Serilog.Sinks.Channels;
+using Serilog.Configuration;
+using Serilog.Events;
+
+namespace Serilog
+{
+	/// <summary>
+	/// Extends <see cref="LoggerConfiguration"/> with methods for configuring asynchronous logging using channels.
+	/// </summary>
+	public static class LoggerConfigurationChannelExtensions
+	{
+		/// <summary>
+		/// Configure a sink to be invoked asynchronously, using a channel.
+		/// </summary>
+		/// <param name="loggerSinkConfiguration">The <see cref="LoggerSinkConfiguration"/> being configured.</param>
+		/// <param name="configure">An action that configures the wrapped sink.</param>
+		/// <returns>A <see cref="LoggerConfiguration"/> allowing configuration to continue.</returns>
+		public static LoggerConfiguration Channel(
+			this LoggerSinkConfiguration loggerSinkConfiguration,
+			Action<LoggerSinkConfiguration> configure)
+		{
+			return LoggerSinkConfiguration.Wrap(
+				loggerSinkConfiguration,
+				wrappedSink => new ChannelSink(wrappedSink),
+				configure,
+				LevelAlias.Minimum,
+				null);
+		}
+	}
+}
